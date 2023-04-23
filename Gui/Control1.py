4441fbe9c4db27ad63fa2d5b94a788decef87de5
[username] <90497265+Savi-Swarup@users.noreply.github.com>
2023-04-23 10:35:35 +0800

    new Controls

--- Gui/Control1.py
+++ Gui/Control1.py
@@ -9,27 +9,63 @@ import struct
 import threading
 import time
 
+
 from button import Button
 
 
 # With these lines:
-ser = serial.Serial(port='/dev/cu.usbserial-1220', baudrate=115200, parity=serial.PARITY_NONE, bytesize=serial.EIGHTBITS)
+ser = serial.Serial(port='COM12', baudrate=9600, parity=serial.PARITY_NONE, bytesize=serial.EIGHTBITS)
 
 # Check if the connection is successful
 
 HEADER = b'\xab'
 FOOTER = b'\xb3'
+forwByte = b'\xA3'
+backByte = b'\xA4'
+upByte = b'\xA5'
+downByte = b'\xA6'
+rightByte = b'\xA7'
+leftByte = b'\xA8'
+servOpenByte = b'\xA9'
+servCloseByte = b'\xA0'
+pitchUpByte = b'\xC1'
+pitchDownByte = b'\xC2'
+rollRightByte = b'\xC3'
+rollLeftByte = b'\xC4'
+precision = b'\xF1'
+regular = b'\xF2'
+rapid = b'\xF3'
+#optional if using IMU:
+hoverByte = b'\xD1'
+stopByte = b'\xB5'
+buffer = []
+speed = 2 #1 = precision, 2 = regular, 3 = precision
+speedByte = None
 
-def foo(msg):
-    if type(msg) == bytes:
-        msg = msg[0]
-    out = 0b0
-    for i in range(8):
-        bit = msg & 1
-        msg >>= 1
-        out |= (0 if bit else 1)
-        out <<= 1
-    return bytes([out >> 1])
+
+def sendSerial(data):
+    ser.write(HEADER)
+    match speed:
+        case 1:
+            speedByte = precision
+        case 2:
+            speedByte = regular
+        case 3:
+            speedByte = rapid
+    ser.write(speedByte)
+    ser.write(data)
+    ser.write(FOOTER)
+
+# def foo(msg):
+#     if type(msg) == bytes:
+#         msg = msg[0]
+#     out = 0b0
+#     for i in range(8):
+#         bit = msg & 1
+#         msg >>= 1
+#         out |= (0 if bit else 1)
+#         out <<= 1
+#     return bytes([out >> 1])
 
 class MainWindow(QMainWindow):
 
@@ -55,7 +91,7 @@ class MainWindow(QMainWindow):
         self.logo = QLabel(self)
         self.pixmap = QPixmap('logo.png')
         self.logo.setPixmap(self.pixmap)
-        self.logo.resize(self.pixm  ap.width(),
+        self.logo.resize(self.pixmap.width(),
                           self.pixmap.height())
   
         # self.label.setFont(QFont('Arial', 25))
@@ -87,73 +123,133 @@ class MainWindow(QMainWindow):
     def handle_received_data(self, data):
         print(data)
 
+        
     # Layout 1
     def keyPressEvent(self, e):
         if e.isAutoRepeat():
 
             if e.key() == Qt.Key_W: # FORWARD
-                
-                ser.write(foo(struct.pack("<c", foo(HEADER))))
-                ser.write(struct.pack("<c", foo(1)))
-                ser.write(struct.pack("<c", foo(FOOTER)))
+                # ser.write(HEADER)
+                # ser.write(forwByte)
+                # ser.write(FOOTER)
+                #buffer.append(forwByte)    
+                sendSerial(forwByte)            
+                    #ser.write(foo(struct.pack("<c", foo(HEADER))))
+                    #ser.write(struct.pack("<c", foo(1)))
+                    #ser.write(struct.pack("<c", foo(FOOTER)))
 
-                # val = struct.pack(">cHc", HEADER, 0, FOOTER)
-                # print(f"writing {val}")
-                # ser.write(val)
+                    # val = struct.pack(">cHc", HEADER, 0, FOOTER)
+                    # print(f"writing {val}")
+                    # ser.write(val)
 
 
 
             if e.key() == Qt.Key_A: # LEFT
-                ser.write(struct.pack(">cHc", HEADER, 2, FOOTER))
+                # ser.write(HEADER)
+                # ser.write(leftByte)
+                # ser.write(FOOTER)
+                #buffer.append(leftByte)
+                sendSerial(leftByte)
+                #     #ser.write(struct.pack(">cHc", HEADER, 2, FOOTER))
 
             if e.key() == Qt.Key_S: # BACKWARD
-                ser.write(struct.pack(">cHc", HEADER, 1, FOOTER))
+                # ser.write(HEADER)
+                # ser.write(backByte)
+                # ser.write(FOOTER)
+                #buffer.append(backByte)
+                sendSerial(backByte)
+                #     #ser.write(struct.pack(">cHc", HEADER, 1, FOOTER))
 
             if e.key() == Qt.Key_D: # RIGHT
-                ser.write(struct.pack(">cHc", HEADER, 3, FOOTER))
-          
+                # ser.write(HEADER)
+                # ser.write(rightByte)
+                # ser.write(FOOTER)
+                #buffer.append(rightByte)
+                sendSerial(rightByte)
+                #     #ser.write(struct.pack(">cHc", HEADER, 3, FOOTER))
+            
             if e.key() == Qt.Key_U: #UP
-                ser.write(struct.pack(">cHc", HEADER, 5, FOOTER))
+                # ser.write(HEADER)
+                # ser.write(upByte)
+                # ser.write(FOOTER)
+                #buffer.append(upByte)
+                sendSerial(upByte)
+                #     #ser.write(struct.pack(">cHc", HEADER, 5, FOOTER))
 
             if e.key() == Qt.Key_I: # DOWN
-                ser.write(struct.pack(">cHc", HEADER, 4, FOOTER))
-            if e.key() == Qt.Key_X: # OFF
-                ser.write(struct.pack(">cHc", HEADER, 7, FOOTER))
-          
-            # if e.key() == Qt.Key_T:
-            #     s = "CAMUP"
-            #     ser.write(struct.pack(">cHc", HEADER, 7, FOOTER))
-         
-            # if e.key() == Qt.Key_Y:
-            #     s = "CAMDOWN"   
-            #     ser.write(struct.pack(">cHc", HEADER, 8, FOOTER))
-           
-            # if e.key() == Qt.Key_N:
-            #     s = "CLAWOPEN"
-            #     ser.write(struct.pack(">cHc", HEADER, 9, FOOTER))
+                # ser.write(HEADER)
+                # ser.write(downByte)
+                # ser.write(FOOTER)
+                #buffer.append(downByte)
+                sendSerial(downByte)
+                #     #ser.write(struct.pack(">cHc", HEADER, 4, FOOTER))
+            #if e.key() == Qt.Key_X: # OFF
+                # ser.write(HEADER)
+                # ser.write(stopByte)
+                # ser.write(FOOTER)
+                #buffer.append(stopByte)
+                #     #ser.write(struct.pack(">cHc", HEADER, 7, FOOTER))
+            
+                # if e.key() == Qt.Key_T:
+                #     s = "CAMUP"
+                #     ser.write(struct.pack(">cHc", HEADER, 7, FOOTER))
+            
+                # if e.key() == Qt.Key_Y:
+                #     s = "CAMDOWN"   
+                #     ser.write(struct.pack(">cHc", HEADER, 8, FOOTER))
+            
+            if e.key() == Qt.key.T:
+                sendSerial(pitchUpByte)
+            if e.key() == Qt.key.Y:
+                sendSerial(pitchDownByte)
+            if e.key() == Qt.key.G:
+                sendSerial(rollLeftByte)
+            if e.key() == Qt.key.H:
+                sendSerial(rollRightByte)
+            if e.key() == Qt.Key_N:
+                # ser.write(HEADER)
+                # ser.write(servOpenByte)
+                # ser.write(FOOTER)
+                #buffer.append(servOpenByte)
+                sendSerial(servOpenByte)
+                # #     s = "CLAWOPEN"
+                #     ser.write(struct.pack(">cHc", HEADER, 9, FOOTER))
 
-            # if e.key() == Qt.Key_M:
-            #     s = "CLAWCLOSE"   
-            #     ser.write(struct.pack(">cHc", HEADER, 10, FOOTER))
-           
-            # if e.key() == Qt.Key_1:
-            #     s = "Inc"
-            #     ser.write(s.encode())
-            # if e.key() == Qt.Key_2:
-            #     s = "Dec"
-            #     ser.write(s.encode())
-            # if e.key() == Qt.Key_Z:
-            #     s = "TiltUp"
-            #     ser.write(s.encode())
-            # if e.key() == Qt.Key_X:
-            #     s = "TiltDown"
-            #     ser.write(s.encode())
+            if e.key() == Qt.Key_M:
+                # ser.write(HEADER)
+                # ser.write(servCloseByte)
+                # ser.write(FOOTER)
+                #buffer.append(servCloseByte)
+                sendSerial(servCloseByte)
+                # #     s = "CLAWCLOSE"   
+                #     ser.write(struct.pack(">cHc", HEADER, 10, FOOTER))
+            
+                # if e.key() == Qt.Key_1:
+                #     s = "Inc"
+                #     ser.write(s.encode())
+                # if e.key() == Qt.Key_2:
+                #     s = "Dec"
+                #     ser.write(s.encode())
+                # if e.key() == Qt.Key_Z:
+                #     s = "TiltUp"
+                #     ser.write(s.encode())
+                # if e.key() == Qt.Key_X:
+                #     s = "TiltDown"
+                #     ser.write(s.encode())
+            if e.key() == Qt.Key_O:
+                speed = 3
+            if e.key() == Qt.Key_K:
+                speed = 2
+            if e.key() == Qt.Key.L:
+                speed = 1
+
 
                 
 
-    # def keyReleaseEvent(self, e):
-    #     s = "OFF"
-    #     ser.write(s.encode())
+    def keyReleaseEvent(self, e):
+        sendSerial(stopByte)
+        #s = "OFF"
+        #ser.write(s.encode())
 
 
 
@@ -248,6 +344,3 @@ if __name__ == '__main__':
     window.show()
 
     app.exec()
-
-    
-
